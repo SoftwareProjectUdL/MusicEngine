8811d2f3e126d106e5ba8b9735399546feb1f459
Marc <121486915+bustamens@users.noreply.github.com>
2023-05-20 17:30:20 +0200

    ME App Public

--- MusicEngineApp/public/urls.py
+++ MusicEngineApp/public/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', home_view, name="home"),
+    path('reserva', reserva_view, name="reserva"),
+    path('reserva/save', reserva_save, name="reserva_save"),
 ]
